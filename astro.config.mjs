3ddd3803259094bae932bb907b50894d6f1597c9
Kapil Sharma <kapil.sharma@leanix.net>
2026-02-26 20:22:00 +0100

    feat: configure site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://kapil5harma.github.io',
 	integrations: [mdx(), sitemap()],
 });
